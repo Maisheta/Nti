e6549220503ed1b0af0b947c7b0032756bc42ed9
Mai Sheta <120672459+Maisheta@users.noreply.github.com>
2025-05-03 11:55:30 +0300

    privacy_settings_screen.dart

--- lib/screens/privacy_settings_screen.dart
+++ lib/screens/privacy_settings_screen.dart
@@ -26,53 +26,33 @@ class PrivacySettingsScreen extends StatelessWidget {
           ListTile(
             title: Text('Last seen and online'),
             subtitle: Text('Everyone'),
-            onTap: () {
-              // Navigate to selection screen
-            },
             trailing: Icon(Icons.arrow_forward_ios, size: 16),
           ),
           ListTile(
             title: Text('Profile photo'),
             subtitle: Text('Everyone'),
-            onTap: () {
-              // Navigate to selection screen
-            },
             trailing: Icon(Icons.arrow_forward_ios, size: 16),
           ),
           ListTile(
             title: Text('About'),
             subtitle: Text('Everyone'),
-            onTap: () {
-              // Navigate to selection screen
-            },
             trailing: Icon(Icons.arrow_forward_ios, size: 16),
           ),
-          SwitchListTile(
+          ListTile(
             title: Text('Read receipts'),
             subtitle: Text(
               "If turned off, you won't send or receive Read receipts. Read receipts are always sent for group chats.",
               style: TextStyle(fontSize: 12),
             ),
-            value: true, // Default value for the switch
-            onChanged: (bool value) {
-              // Handle switch toggle
-            },
-            activeColor: Colors.teal,
           ),
           ListTile(
             title: Text('Disappearing messages'),
             subtitle: Text('Default message timer'),
-            onTap: () {
-              // Navigate to selection screen
-            },
             trailing: Icon(Icons.arrow_forward_ios, size: 16),
           ),
           ListTile(
             title: Text(''),
             subtitle: Text('Off'),
-            onTap: () {
-              // Navigate to selection screen
-            },
             trailing: Icon(Icons.arrow_forward_ios, size: 16),
           ),
         ],
